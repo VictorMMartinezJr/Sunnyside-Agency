2292490eb91a87dc39ba42c8d0af26a112c17e70
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-10-06 19:20:04 -0400

    add mobile nav and add mobile nav toggle

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const navMenuIcon = document.querySelector(".nav__menuIcon");
+const navLinks = document.querySelector(".nav__links");
+
+console.log(navLinks);
+
+navMenuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
